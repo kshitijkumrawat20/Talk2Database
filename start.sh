acfbcf08367b9b52c3f7142467796893b99a581b
kshtitijkumrawat20 <kshitijk146@gmail.com>
2025-02-09 13:59:39 +0530

    title changed

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 uvicorn app.main:app --host 0.0.0.0 --port 8000 &
 
 # Start the Streamlit frontend server
-streamlit run app/frontend/frontend_app.py --server.port 80 --server.enableCORS false
+streamlit run app/frontend/Talk2SQL.py --server.port 80 --server.enableCORS false
