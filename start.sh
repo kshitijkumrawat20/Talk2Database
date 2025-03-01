71868d297fb1f7957da4809e9e2e4acbb774866f
kshtitijkumrawat20 <kshitijk146@gmail.com>
2025-03-01 16:58:09 +0530

    error fixed

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 uvicorn app.main:app --host 0.0.0.0 --port 8000 &
 
 # Start the Streamlit frontend server
-streamlit run app/frontend/Talk2SQL.py --server.port 80 --server.enableCORS false
+streamlit run frontend/app.py --server.port 80 --server.address 0.0.0.0
