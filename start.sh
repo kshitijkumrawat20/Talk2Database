26f9d1266b14e3dac3aa5b2442a7da7711e2b650
kshtitijkumrawat20 <kshitijk146@gmail.com>
2025-03-01 14:50:14 +0530

    error fixed

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Start FastAPI in the background
-uvicorn app.main:app --host 0.0.0.0 --port 8000 &
+uvicorn app.main:app --reload
 
 # # Wait for FastAPI to be available
 # until curl -s http://localhost:8000/docs > /dev/null; do
