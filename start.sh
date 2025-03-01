b6d217b216e80136ca3aa1791d96d677f12eb67f
kshtitijkumrawat20 <kshitijk146@gmail.com>
2025-03-01 14:45:05 +0530

    error fixed

--- start.sh
+++ start.sh
@@ -3,5 +3,11 @@
 # Start FastAPI in the background
 uvicorn app.main:app --host 0.0.0.0 --port 8000 &
 
+# Wait for FastAPI to be available
+until curl -s http://localhost:8000/docs > /dev/null; do
+  echo "Waiting for FastAPI to start..."
+  sleep 3
+done
+
 # Start Streamlit
-streamlit run app/frontend/Talk2SQL.py --server.port 80 --server.enableCORS false
+streamlit run app/frontend/Talk2SQL.py --server.port 80 --server.enableCORS false --server.enableXsrfProtection false
