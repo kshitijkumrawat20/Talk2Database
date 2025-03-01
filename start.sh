638bf321e06904887b1e36dae825d2f36cef312d
kshtitijkumrawat20 <kshitijk146@gmail.com>
2025-03-01 23:09:28 +0530

    testing new deployement

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Start the FastAPI backend server
-uvicorn app.main:app --host 0.0.0.0 --port 8000 &
-
-# Start the Streamlit frontend server
-streamlit run app/frontend/Talk2SQL.py --server.port 80 --server.enableCORS false
