5a2d8c53a70c2e2ce53e81f80ad166cd80b231bf
kshtitijkumrawat20 <kshitijk146@gmail.com>
2025-03-01 12:55:29 +0530

    Fixed issues

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 # Start the FastAPI backend server
 uvicorn app.main:app --host 0.0.0.0 --port 8000 &
 
-# Start the Streamlit frontend server
+# Start the Streamlit frontend server and commit 
 streamlit run app/frontend/Talk2SQL.py --server.port 80 --server.enableCORS false
